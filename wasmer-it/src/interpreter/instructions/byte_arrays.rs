ba64f5e8e47d9d802a87aa9d749a047678ff9d15
Valery Antopol <valery.antopol@gmail.com>
2022-01-12 17:11:58 +0300

    fix byte_array_lift_memory and return old order of functions in byte_arrays

--- wasmer-it/src/interpreter/instructions/byte_arrays.rs
+++ wasmer-it/src/interpreter/instructions/byte_arrays.rs
@@ -10,6 +10,70 @@ use crate::{
 use std::convert::TryInto;
 
 executable_instruction!(
+    byte_array_lift_memory(instruction: Instruction) -> _ {
+        move |runtime| -> _ {
+            let mut inputs = runtime.stack.pop(2).ok_or_else(|| {
+                InstructionError::from_error_kind(
+                    instruction.clone(),
+                    InstructionErrorKind::StackIsTooSmall { needed: 2 },
+                )
+            })?;
+
+            let memory_index = 0;
+            let memory = runtime
+                .wasm_instance
+                .memory(memory_index)
+                .ok_or_else(|| {
+                    InstructionError::from_error_kind(
+                        instruction.clone(),
+                        InstructionErrorKind::MemoryIsMissing { memory_index },
+                    )
+                })?;
+
+            let pointer: usize = to_native::<i32>(inputs.remove(0), instruction.clone())?
+                .try_into()
+                .map_err(|e| (e, "pointer").into())
+                .map_err(|k| InstructionError::from_error_kind(instruction.clone(), k))?;
+            let length: usize = to_native::<i32>(inputs.remove(0), instruction.clone())?
+                .try_into()
+                .map_err(|e| (e, "length").into())
+                .map_err(|k| InstructionError::from_error_kind(instruction.clone(), k))?;
+
+            let memory_view = memory.view();
+
+            if length == 0 {
+                runtime.stack.push(IValue::ByteArray(vec![]));
+
+                return Ok(())
+            }
+            /*
+
+            if memory_view.len() < pointer + length {
+                return instr_error!(
+                    instruction.clone(),
+                    InstructionErrorKind::MemoryOutOfBoundsAccess {
+                        index: pointer + length,
+                        length: memory_view.len(),
+                    }
+                );
+            }*/
+
+            let reader = memory_view.sequential_reader(pointer, length);
+
+            let mut data = Vec::<u8>::default();
+            for _ in 0..length {
+                data.push(reader.read_u8());
+            }
+
+            log::debug!("byte_array.lift_memory: pushing {:?} on the stack", data);
+            runtime.stack.push(IValue::ByteArray(data));
+
+            Ok(())
+        }
+    }
+);
+
+executable_instruction!(
     byte_array_lower_memory(instruction: Instruction) -> _ {
         move |runtime| -> _ {
             let mut inputs = runtime.stack.pop(2).ok_or_else(|| {
@@ -49,56 +113,6 @@ executable_instruction!(
             log::debug!("string.lower_memory: pushing {}, {} on the stack", array_pointer, length);
             runtime.stack.push(IValue::I32(array_pointer as i32));
             runtime.stack.push(IValue::I32(length));
-
-            Ok(())
-        }
-    }
-);
-
-executable_instruction!(
-    byte_array_lift_memory(instruction: Instruction) -> _ {
-        move |runtime| -> _ {
-            let mut inputs = runtime.stack.pop(2).ok_or_else(|| {
-                InstructionError::from_error_kind(
-                    instruction.clone(),
-                    InstructionErrorKind::StackIsTooSmall { needed: 2 },
-                )
-            })?;
-
-            let memory_index = 0;
-            let memory = runtime
-                .wasm_instance
-                .memory(memory_index)
-                .ok_or_else(|| {
-                    InstructionError::from_error_kind(
-                        instruction.clone(),
-                        InstructionErrorKind::MemoryIsMissing { memory_index },
-                    )
-                })?;
-
-            let pointer: usize = to_native::<i32>(inputs.remove(0), instruction.clone())?
-                .try_into()
-                .map_err(|e| (e, "pointer").into())
-                .map_err(|k| InstructionError::from_error_kind(instruction.clone(), k))?;
-            let length: usize = to_native::<i32>(inputs.remove(0), instruction.clone())?
-                .try_into()
-                .map_err(|e| (e, "length").into())
-                .map_err(|k| InstructionError::from_error_kind(instruction.clone(), k))?;
-
-            let memory_view = memory.view();
-
-            if length == 0 {
-                runtime.stack.push(IValue::ByteArray(vec![]));
-
-                return Ok(())
-            }
-
-            let reader = memory_view.sequential_reader(pointer, length);
-
-            let mut data = Vec::<u8>::default();
-            for _ in 0..length {
-                data.push(reader.read_u8());
-            }
 
             Ok(())
         }
